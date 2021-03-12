c6261f773c053ce87463cbcdcdb71889a5b1be22
Mahdyar <hi@mahdyar.me>
2021-03-12 12:57:04 +0330

    feat: add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <Layout :showFooter="false">
+    <div id="not-found">
+      <p>404: not found</p>
+    </div>
+  </Layout>
+</template>
+<style lang="scss">
+#not-found {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  font-size: 2em;
+}
+</style>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Not found - The Goat",
+  },
+};
+</script>
