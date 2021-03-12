6a0e1e40c8e37e7cbb0bad5b0796d9f3c3aa66b0
Mahdyar <hi@mahdyar.me>
2021-03-12 17:42:08 +0330

    feat: add loading for repositories

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,14 +12,59 @@
       />
     </div>
     <div class="repositories">
-      <Repository
-        v-for="repository in repositories"
-        :key="repository.id"
-        :url="repository.html_url"
-        :title="repository.full_name"
-        :description="repository.description"
-        :starCount="repository.stargazers_count"
-      />
+      <div v-if="errored"></div>
+      <div v-if="loading">
+        <div class="repository">
+          <content-placeholders :rounded="true">
+            <h4><content-placeholders-heading /></h4>
+            <p><content-placeholders-text :lines="1" /></p>
+            <div class="stars">
+              <Star />
+              <span class="star-count">0</span>
+            </div>
+          </content-placeholders>
+        </div>
+        <div class="repository">
+          <content-placeholders :rounded="true">
+            <h4><content-placeholders-heading /></h4>
+            <p><content-placeholders-text :lines="1" /></p>
+            <div class="stars">
+              <Star />
+              <span class="star-count">0</span>
+            </div>
+          </content-placeholders>
+        </div>
+        <div class="repository">
+          <content-placeholders :rounded="true">
+            <h4><content-placeholders-heading /></h4>
+            <p><content-placeholders-text :lines="1" /></p>
+            <div class="stars">
+              <Star />
+              <span class="star-count">0</span>
+            </div>
+          </content-placeholders>
+        </div>
+        <div class="repository">
+          <content-placeholders :rounded="true">
+            <h4><content-placeholders-heading /></h4>
+            <p><content-placeholders-text :lines="1" /></p>
+            <div class="stars">
+              <Star />
+              <span class="star-count">0</span>
+            </div>
+          </content-placeholders>
+        </div>
+      </div>
+      <div v-else>
+        <Repository
+          v-for="repository in repositories"
+          :key="repository.id"
+          :url="repository.html_url"
+          :title="repository.full_name"
+          :description="repository.description"
+          :starCount="repository.stargazers_count"
+        />
+      </div>
     </div>
   </Layout>
 </template>
@@ -50,6 +95,7 @@ query {
 <script>
 import Author from "~/components/Author.vue";
 import PostCard from "~/components/PostCard.vue";
+import Star from "~/components/Star.vue";
 import Repository from "~/components/Repository.vue";
 import axios from "axios";
 export default {
@@ -57,10 +103,13 @@ export default {
     Author,
     PostCard,
     Repository,
+    Star,
   },
   data() {
     return {
       repositories: null,
+      loading: true,
+      errored: false,
     };
   },
   created() {
@@ -68,7 +117,11 @@ export default {
       .get(
         "https://api.github.com/orgs/thegoat-ir/repos?type=public&sort=updated&per_page=10&page=1"
       )
-      .then((response) => (this.repositories = response.data));
+      .then((response) => (this.repositories = response.data))
+      .catch((error) => {
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   },
   metaInfo: {
     title: "The greatest of all time",
